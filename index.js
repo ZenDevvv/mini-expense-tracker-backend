829891ddfe88a9314650721ba0076119c4873ea5
ZenDev <ozenandreiii@gmail.com>
2024-09-19 08:06:40 +0800

    database connected

--- index.js
+++ index.js
@@ -19,16 +19,15 @@ app.use(
 );
 
 
-const PORT = process.env.PORT;
-console.log(PORT);
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   ssl: {
-    rejectUnauthorized: true,
+    rejectUnauthorized: false,
     ca: fs.readFileSync("./ca.pem").toString(),
   }
 });
@@ -181,6 +180,6 @@ app.put("/edit/:id", (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
+app.listen(process.env.DB_PORT, () => {
   console.log("Backend Connected");
 });
